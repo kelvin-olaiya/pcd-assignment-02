4569df24ebdbc8cd59e2aa8a710b9e4f24a5bff8
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-05-03 15:22:06 +0200

    improve gradlew 8.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,15 @@ plugins {
     java
     application
 }
+
+application {
+    mainClass.set("HelloWorld")
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.compilerArgs.addAll(listOf("--enable-preview"))
+}
+
+tasks.withType<JavaExec>().configureEach {
+    jvmArguments.addAll("--enable-preview")
+}
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
-    public static void main(String... args) {
-        System.out.println("Hello World!");
+    public static void main(String... args) throws InterruptedException {
+//        System.out.println("Hello World!");
+        Thread
+                .ofVirtual()
+                .name("myVirtualThread-")
+                .start(() -> System.out.println("ciao"));
+        Thread.sleep(1000);
     }
 }
