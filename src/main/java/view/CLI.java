3faf02a52622f4702992f409ff33a9b80038daa3
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2023-05-18 15:01:05 +0200

    chore: display running time

--- src/main/java/view/CLI.java
+++ src/main/java/view/CLI.java
@@ -16,13 +16,17 @@ public class CLI {
     }
 
     public void start(String directory) throws ExecutionException, InterruptedException {
+        long startTime = System.currentTimeMillis();
         Future<Report> futureResult = this.sourceAnalyzer.getReport(new Directory(new File(directory)));
         Report report = futureResult.get();
+        long totalTime = System.currentTimeMillis() - startTime;
         System.out.println(report);
         System.out.println();
         System.out.println("The longest files are:");
         for (var line : report.longestFiles()) {
             System.out.println(line.getX() + "\t" + line.getY() + " lines");
         }
+        System.out.println();
+        System.out.println("Done in " + totalTime + " ms");
     }
 }
